8cafbbd3e946061fd2482010d1ffb364527ce115
JesusJC15 <93460550+JesusJC15@users.noreply.github.com>
2023-10-22 17:20:13 -0500

    Update oservables_y_medidas.py

--- oservables_y_medidas.py
+++ oservables_y_medidas.py
@@ -1,6 +1,4 @@
-import math
 import numpy as np
-
 
 def normalizar(vect):
     """Normaliza un vector
@@ -13,25 +11,38 @@ def normalizar(vect):
     print(norm)
     return vect_n
 
-
 def part_una_linea(vect, pos):
-    """Sistema probabilistico de una linea
+    """Sistema probabilístico de una línea
     (vect, int) -> float
     """
-    pos = vect[pos]
-    pos = (np.linalg.norm(pos)) ** 2
-    vect = (np.linalg.norm(vect)) ** 2
-    prob = np.round(pos / vect, 2)
+    prob = np.round(np.abs(vect[pos])**2, 2)
     return prob
 
-
 def ampli_tran(vect1, vect2):
-    """Calcula la probabilidad de la amplitud de transicion de un vector a otro
-    (vect, vect) -> cplx
+    """Calcula la probabilidad de la amplitud de transición de un vector a otro
+    (vect1, vect2) -> float
     """
     vect1 = normalizar(vect1)
     vect2 = normalizar(vect2)
     bra = np.conjugate(np.transpose(vect2))
     prod_int = np.dot(bra, vect1)
-    prob = np.linalg.norm(prod_int)**2
-    return np.round(prob, 0)
+    prob = np.round(np.abs(prod_int)**2, 2)
+    return prob
+
+def media_varianza(mat_obs, vect_est):
+    """Calcula la media y la varianza del observable en el estado dado.
+    (mat_obs, vect_est) -> (float, float)
+    """
+    val_esp = np.real(np.dot(vect_est.conj().T, np.dot(mat_obs, vect_est)))
+    mat_obs_cua = np.dot(mat_obs, mat_obs)
+    val_esp_cua = np.real(np.dot(vect_est.conj().T, np.dot(mat_obs_cua, vect_est)))
+    vari = val_esp_cua - val_esp**2
+    return val_esp, vari
+
+def val_y_vect_prop(mat_obs, vect_est):
+    """Calcula los valores propios del observable y la probabilidad de que el sistema transite a alguno de los vectores propios después de la observación.
+    (mat_obs, vect_est) -> (np.ndarray, np.ndarray)
+    """
+    val_pro, vect_pro = np.linalg.eig(mat_obs)
+    proba = np.abs(np.dot(vect_pro.conj().T, vect_est)) ** 2
+    return val_pro, proba
